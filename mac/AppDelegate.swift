71abaae5974ed6756eb24822a2cc4d73b11db250
Marquis Kurt <software@marquiskurt.net>
2018-11-18 20:11:35 -0500

    Initial Commit

---
+++ mac/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  mac
+//
+//  Created by Marquis Kurt on 11/18/18.
+//  Copyright © 2018 Marquis Kurt. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
